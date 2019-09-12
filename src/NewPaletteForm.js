c616eec0d3f738bd6c5f6b267ea2b684c8d4cef3
Axel <axosar@gmail.com>
2019-09-12 15:42:05 -0500

    Connect color picker to button
    Connect color picker to button

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -78,9 +78,13 @@ class NewPaletteForm extends Component {
     super(props);
     this.state = {
       open: false,
-      currentColor: 'teal'
+      currentColor: 'teal',
+      colors: [
+        "purple", "#345764"
+      ]
     };
     this.updateCurrentColor = this.updateCurrentColor.bind(this);
+    this.addNewColor = this.addNewColor.bind(this);
   }
 
   handleDrawerOpen = () => {
@@ -93,7 +97,11 @@ class NewPaletteForm extends Component {
 
   updateCurrentColor(newColor){
     this.setState({currentColor: newColor.hex});
-  }
+  };
+
+  addNewColor(){
+    this.setState({colors: [...this.state.colors, this.state.currentColor]})
+  };
 
   render() {
     const { classes } = this.props;
@@ -154,6 +162,7 @@ class NewPaletteForm extends Component {
             variant='contained' 
             color='primary'
             style={{backgroundColor: this.state.currentColor}}
+            onClick={this.addNewColor}
           >
             Add Color
           </Button>
@@ -164,6 +173,11 @@ class NewPaletteForm extends Component {
           })}
         >
           <div className={classes.drawerHeader} />
+          <ul>
+            {this.state.colors.map(color => (
+              <li style={{backgroundColor: color}}>{color}</li>
+            ))}
+          </ul>
         </main>
       </div>
     );
