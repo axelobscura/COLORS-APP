75fb554c8f7d9d081bb576cdf92b89434698ab14
Axel <axosar@gmail.com>
2019-09-13 13:00:32 -0500

    Passing value to parent
    Submiting new palette to parent component

--- src/App.js
+++ src/App.js
@@ -8,18 +8,31 @@ import NewPaletteForm from './NewPaletteForm';
 import {generatePalette} from './colorHelpers';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.savePalette = this.savePalette.bind(this);
+  }
+
   findPalette(id){
     return seedColors.find(function(palette){
       return palette.id === id;
     })
-  }
+  };
+
+  savePalette(newPalette){
+    console.log(newPalette)
+  };
+
   render(){
     return (
       <Switch>
         <Route 
           exact 
           path="/palette/new" 
-          render={() => <NewPaletteForm/>}
+          render={routeProps => (
+            <NewPaletteForm savePalette={this.savePalette} {...routeProps} />
+          )}
         />
         <Route 
           exact
