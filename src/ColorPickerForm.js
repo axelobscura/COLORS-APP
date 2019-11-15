68708267cdc956b0422bb14dd6b8d32732015ac9
Axel <axosar@gmail.com>
2019-11-14 18:56:16 -0600

    ColorPicker

---
+++ src/ColorPickerForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ColorPickerForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+ 
+export default ColorPickerForm;
