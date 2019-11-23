be0a7723d6b57f9abaa7528f8c44fe6c032cdab1
Axel <axosar@gmail.com>
2019-11-22 18:49:07 -0600

    Add responsive colorboxes

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -1,7 +1,8 @@
 import chroma from "chroma-js";
-export default {
+import sizes from './Sizes';
+const styles = {
   ColorBox: {
-    width: "20%",
+    width: "25%",
     height: props => (props.showingFullPalette ? "25%" : "50%"),
     margin: "0 auto",
     display: "inline-block",
@@ -10,6 +11,14 @@ export default {
     marginBottom: "-3.5px",
     "&:hover button": {
       opacity: 1
+    },
+    [sizes.down("lg")]: {
+      width: "25%",
+      height: props => (props.showingFullPalette ? "20%" : "10%"),
+    },
+    [sizes.down("md")]: {
+      width: "50%",
+      height: props => (props.showingFullPalette ? "5%" : "10%"),
     }
   },
   copyText: {
@@ -117,3 +126,5 @@ export default {
     transitionDelay: "0.3s"
   }
 };
+
+export default styles;
