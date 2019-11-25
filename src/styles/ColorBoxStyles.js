6897f2b1e978b138019e627463a18ac9b5939186
Axel <axosar@gmail.com>
2019-11-25 12:12:57 -0600

    Make colorbix responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -14,10 +14,14 @@ const styles = {
     },
     [sizes.down("lg")]: {
       width: "25%",
-      height: props => (props.showingFullPalette ? "20%" : "10%"),
+      height: props => (props.showingFullPalette ? "20%" : "33.3333%"),
     },
     [sizes.down("md")]: {
       width: "50%",
+      height: props => (props.showingFullPalette ? "10%" : "20%"),
+    },
+    [sizes.down("xs")]: {
+      width: "100%",
       height: props => (props.showingFullPalette ? "5%" : "10%"),
     }
   },
@@ -111,7 +115,10 @@ const styles = {
       textAlign: "center",
       marginBottom: "0",
       padding: "1rem",
-      textTransform: "uppercase"
+      textTransform: "uppercase",
+      [sizes.down("xs")]: {
+        fontSize: "5rem"
+      }
     },
     "& p": {
       fontSize: "2rem",
