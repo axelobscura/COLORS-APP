f0038987b34f31f6259800ed5866d7ffff70bdac
Axel <axosar@gmail.com>
2019-11-28 18:36:46 -0600

    Updates

--- precache-manifest.6ac8eed9e2be45d06875406410f50074.js
+++ precache-manifest.6ac8eed9e2be45d06875406410f50074.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "92ca41dbe9a1aab6c696143de1a6ed4d",
+    "revision": "f223378e5e62c217ec462930958e291a",
     "url": "/COLORS-APP/index.html"
   },
   {
-    "revision": "a7f0c5864eed6ab8743e",
+    "revision": "84d9a83b38b09b91decd",
     "url": "/COLORS-APP/static/css/2.e337d257.chunk.css"
   },
   {
-    "revision": "a468b9b1d94828982a4c",
+    "revision": "72e5d9cf1f7fdd9b5041",
     "url": "/COLORS-APP/static/css/main.7537cb2b.chunk.css"
   },
   {
-    "revision": "a7f0c5864eed6ab8743e",
-    "url": "/COLORS-APP/static/js/2.78489056.chunk.js"
+    "revision": "84d9a83b38b09b91decd",
+    "url": "/COLORS-APP/static/js/2.053087a7.chunk.js"
   },
   {
-    "revision": "a468b9b1d94828982a4c",
-    "url": "/COLORS-APP/static/js/main.8188e5a4.chunk.js"
+    "revision": "72e5d9cf1f7fdd9b5041",
+    "url": "/COLORS-APP/static/js/main.cc036b80.chunk.js"
   },
   {
     "revision": "d0889f82b352eb788784",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/COLORS-APP/precache-manifest.34241d0c6f102a78215a26db03cd5e7b.js"
+  "/COLORS-APP/precache-manifest.6ac8eed9e2be45d06875406410f50074.js"
 );
 
 self.addEventListener('message', (event) => {
